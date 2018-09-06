fcd2d45273360d802b3e0b7814d35d64880c8f2b
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-09-06 15:34:56 +0200

    Show pattern on the launchpad

--- scroll/script/test.rb
+++ scroll/script/test.rb
@@ -1,8 +1,33 @@
 require 'launchpad/device'
 
+def to_hash(letter)
+  case letter
+  when "*"
+    { :red => :high }
+  else
+    { }
+  end
+end
+
+base_pattern = [
+  "*..*..*.",
+  ".*..*..*",
+  "..*..*..",
+  "*..*..*.",
+  ".*..*..*",
+  "..*..*..",
+  "*..*..*.",
+  ".*..*..*",
+]
+
+pattern = base_pattern
+            .map { |word| word.split('') }
+            .flatten()
+            .map { |letter| to_hash(letter) }
+
 device = Launchpad::Device.new({:device_name => "Launchpad Mini MIDI 1"})
 device.test_leds
 sleep 1
 device.reset
 sleep 1
-device.change :grid, :x => 4, :y => 4, :red => :high, :green => :low
+device.change_all pattern
