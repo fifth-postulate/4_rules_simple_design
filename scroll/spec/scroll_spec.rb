bc565e73abcfc985aff36084c871886d7f58326d
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-09-10 15:07:36 +0200

    Test the advance method

--- scroll/spec/scroll_spec.rb
+++ scroll/spec/scroll_spec.rb
@@ -1,11 +1,14 @@
 require "scroll"
 
 RSpec.describe Scroll do
+  before(:each) do
+    @scroller = Scroll.new("abc")
+  end
+
   it "should send a display message to a receiver" do
     receiver = spy("receiver")
-    scroller = Scroll.new("abc")
 
-    scroller.show_on(receiver)
+    @scroller.show_on(receiver)
 
     expect(receiver).to have_received(:show).with([
                                                     '........',
@@ -17,6 +20,24 @@ RSpec.describe Scroll do
                                                     '*...*.*.',
                                                     '*...*.**',
                                                    ])
+  end
+
+  it "should send a display message to a receiver after an advance" do
+    receiver = spy("receiver")
+
+    @scroller.advance
+    @scroller.show_on(receiver)
+
+    expect(receiver).to have_received(:show).with([
+                                                    '........',
+                                                    '........',
+                                                    '.*...***',
+                                                    '*.*..*..',
+                                                    '...*.***',
+                                                    '****.*..',
+                                                    '...*.*..',
+                                                    '...*.***',
+                                                  ])
   end
 end
 
